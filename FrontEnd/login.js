a7eea9ec1113e4d97a98838c7139bcf2bd9c7b1d
Nicolas Kulpa-Hurault <nicolas.kulpa-hurault@hotmail.com>
2023-06-09 09:39:39 +0200

    correction du login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,10 +1,14 @@
+
 const errorMessage = document.getElementById("error-msg-login");
+
 
 document.getElementById("btn-login").addEventListener("click", async (e) => {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     e.preventDefault();
-    const response = await fetch("http://localhost:5678/api/users/login", {
+
+    
+    const response = await fetch("http://localhost:5678/api/users/login", {// Effectue une requête POST asynchrone vers l'API de connexion utilisateur
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
@@ -12,15 +16,15 @@ document.getElementById("btn-login").addEventListener("click", async (e) => {
         method: "POST",
         body: JSON.stringify({ email: email, password: password }),
     });
-
     const body = await response.json();
-    if (body.token === undefined) {
-        errorMessage.textContent = "E-mail ou Mot de passe incorrect";
-    } 
-    else {
-        setLocalStorageItem("token", body.token);
+
+   
+    if (body.token === undefined) { // Vérifie le token token
+        errorMessage.textContent = "E-mail ou Mot de passe incorrect";// cas ou pas de Token valide
+    } else {
+        setLocalStorageItem("token", body.token);// stock le token dans le local storage
         window.location.href = "index.html";
-      }
+    }
 });
 
 function setLocalStorageItem(key, content) {
